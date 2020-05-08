5c601df529b1753302aad0dcca294efab8a87d8d
Cloiw <marielys11@yahoo.com>
2020-05-08 00:12:05 -0400

    Entorno de trabajo y componente header

---
+++ src/components/Formulario.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Formulario = () => {
+  return ( 
+    <div className="row">
+      <div className="col s12 m8 offset-m2">
+        <form>
+          <h1>Encuentra Noticias por Categorías</h1>
+        </form>
+      </div>
+    </div>
+
+   );
+}
+ 
+export default Formulario
